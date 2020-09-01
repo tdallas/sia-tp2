331aab7e78c551575b969b81522253bbc47c9677
tdallas <tdallas@itba.edu.ar>
2020-09-01 16:26:28 -0300

    added uniform crossover with p = 0.5

--- src/main/java/crossovers/UniformCrossover.java
+++ src/main/java/crossovers/UniformCrossover.java
@@ -2,11 +2,27 @@ package crossovers;
 
 import items.Item;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class UniformCrossover extends Crossover {
+
     @Override
     public List<List<Item>> crossover(List<Item> from, List<Item> to) {
-        return null;
+        final Random random = new Random();
+        final List<Item> firstAlleles = new ArrayList<>();
+        final List<Item> secondAlleles = new ArrayList<>();
+        for (int i = 0 ; i < MAX_ALLELES ; i++) {
+            if (random.nextInt() % 2 == 0) {
+                firstAlleles.add(from.get(i));
+                secondAlleles.add(to.get(i));
+            } else {
+                firstAlleles.add(to.get(i));
+                secondAlleles.add(from.get(i));
+            }
+        }
+        return Arrays.asList(firstAlleles, secondAlleles);
     }
 }
