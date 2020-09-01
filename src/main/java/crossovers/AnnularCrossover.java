6c87c8410da3f33a1e14adf4f5703c1cda746a43
tdallas <tdallas@itba.edu.ar>
2020-09-01 19:27:32 -0300

    added annular crossover

--- src/main/java/crossovers/AnnularCrossover.java
+++ src/main/java/crossovers/AnnularCrossover.java
@@ -2,12 +2,25 @@ package crossovers;
 
 import items.Item;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-// FIXME
 public class AnnularCrossover extends Crossover {
+
     @Override
     public List<List<Item>> crossover(final List<Item> from, final List<Item> to) {
-        return null;
+        final int secondCrossoverPoint = getCrossoverPoint();
+        int firstCrossoverPoint = (int) Math.ceil((double) secondCrossoverPoint/2);
+        final List<Item> firstChildAlleles = getAlleles(from, to, firstCrossoverPoint, secondCrossoverPoint);
+        final List<Item> secondChildAlleles = getAlleles(to, from, firstCrossoverPoint, secondCrossoverPoint);
+        return Arrays.asList(firstChildAlleles, secondChildAlleles);
+    }
+
+    private List<Item> getAlleles(List<Item> from, List<Item> to, int mediumPoint, int toPoint) {
+        final List<Item> newAlleles = new ArrayList<>(to.subList(0, mediumPoint));
+        newAlleles.addAll(from.subList(mediumPoint, toPoint));
+        newAlleles.addAll(to.subList(toPoint, MAX_ALLELES));
+        return newAlleles;
     }
 }
