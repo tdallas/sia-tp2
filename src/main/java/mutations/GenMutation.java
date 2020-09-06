4071906de855212b8db7c1eac4d15f2cc4a697a1
tdallas <tdallas@itba.edu.ar>
2020-09-06 16:53:36 -0300

    fix gen mutation

--- src/main/java/mutations/GenMutation.java
+++ src/main/java/mutations/GenMutation.java
@@ -11,14 +11,19 @@ import java.util.Random;
 @Getter
 public class GenMutation extends Mutation {
 
-    public GenMutation(Random random) {
+    private final double probability;
+
+    public GenMutation(final double probability, final Random random) {
         super(random);
+        this.probability = probability;
     }
 
     @Override
     public List<Allele> mutate(final List<Allele> alleles, final ItemsProvider itemsProvider) {
-        int randomAllele = getRandom().nextInt(MAX_ALLELES);
-        mutateAtPosition(AlleleType.getAlleleType(randomAllele), alleles, itemsProvider);
+        if (getRandom().nextDouble() < probability) {
+            int randomAllele = getRandom().nextInt(MAX_ALLELES);
+            mutateAtPosition(AlleleType.getAlleleType(randomAllele), alleles, itemsProvider);
+        }
         return alleles;
     }
 }
